8d60b393a5c737c50f44cc0e1d039a372fa61bfb
Stuart Shannon <saraandstuart@gmail.com>
2015-04-19 09:32:46 +0100

    Update SAPTest.java

--- algorithms2/test/SAPTest.java
+++ algorithms2/test/SAPTest.java
@@ -9,43 +9,52 @@ import org.junit.Test;
  */
 public class SAPTest {
     
+    private static final String RESOURCE_PATH = "wordnet-testing";
+    private static final String DIGRAPH_1_PATH = RESOURCE_PATH + "/digraph1.txt";
+    private static final String DIGRAPH_2_PATH = RESOURCE_PATH + "/digraph2.txt";
+    
     @Test(expected=IndexOutOfBoundsException.class)
     public void shouldThrowIndexOutOfBoundsExceptionForInvalidArgumentLessThanZero() {
         //when
-        digraph1(-1, 1, 1, 1);
+        digraph1(-1, 1, 1, 1, DIGRAPH_1_PATH);
         //then exception
     }
     
     @Test(expected=IndexOutOfBoundsException.class)
     public void shouldThrowIndexOutOfBoundsExceptionForInvalidArgumentGreaterThanV() {
         //when
-        digraph1(3, 14, 1, 1);
+        digraph1(3, 14, 1, 1, DIGRAPH_1_PATH);
         //then exception
     }
     
     @Test
     public void test1ForDigraph1() {
-        digraph1(3, 11, 4, 1);
+        digraph1(3, 11, 4, 1, DIGRAPH_1_PATH);
     }
     
     @Test
     public void test2ForDigraph1() {
-        digraph1(9, 12, 3, 5);
+        digraph1(9, 12, 3, 5, DIGRAPH_1_PATH);
     }
     
     @Test
     public void test3ForDigraph1() {
-        digraph1(7, 2, 4, 0);
+        digraph1(7, 2, 4, 0, DIGRAPH_1_PATH);
     }
     
     @Test
     public void test4ForDigraph1() {
-        digraph1(1, 6, -1, -1);
+        digraph1(1, 6, -1, -1, DIGRAPH_1_PATH);
     }
     
-    private void digraph1(int v, int w, int expectedLength, int expectedAncestor) {
+    @Test
+    public void test1ForDigraph2() {
+        digraph1(1, 5, 2, 0, DIGRAPH_2_PATH);
+    }
+    
+    private void digraph1(int v, int w, int expectedLength, int expectedAncestor, String resourcePath) {
         //given
-        In in = new In("wordnet-testing/digraph1.txt");
+        In in = new In(resourcePath);
         Digraph G = new Digraph(in);
         SAP sap = new SAP(G);
         
